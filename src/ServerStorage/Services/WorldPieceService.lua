936d3c9fbce96fb794f956c39ea9e14fecf4633b
realcdub <chwh1021@gmail.com>
2023-07-02 03:07:00 -0400

    legal moves to workspace board test

---
+++ src/ServerStorage/Services/WorldPieceService.lua
@@ -0,0 +1,19 @@
+--// Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+
+local Knit = require(ReplicatedStorage.Shared.Packages.Knit)
+
+local WorldPieceService = Knit.CreateService {
+    Name = "PieceService",
+    Client = {},
+}
+
+function WorldPieceService:MoveWorldPiece(WorldPiece : BasePart, TargetSquareCFrame : CFrame)
+    local MoveInfo = TweenInfo.new(1, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, false, 0)
+    local MoveProperties = {CFrame = TargetSquareCFrame + Vector3.new(0, WorldPiece.Size / 2, 0)}
+    local MoveTween = TweenService:Create(WorldPiece, MoveInfo, MoveProperties)
+    MoveTween:Play()
+end
+
+return WorldPieceService
