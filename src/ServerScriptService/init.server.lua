000f049b5ef495b3d0e1665a337d00650047b5ef
realcdub <chwh1021@gmail.com>
2023-07-02 03:05:36 -0400

    server init

--- src/ServerScriptService/init.server.lua
+++ src/ServerScriptService/init.server.lua
@@ -5,8 +5,12 @@ local ServerStorage = game:GetService("ServerStorage")
 --// Utility
 local Knit = require(ReplicatedStorage.Shared.Packages.Knit)
 
-Knit.AddServicesDeep(ServerStorage.Server.Services)
-require(ServerStorage.Server.Classes.Match)
+for _, Service in pairs(ServerStorage.Server.Services:GetDescendants()) do
+    if (Service.Name:match("Service$")) then
+        print(Service.Name .. " Loaded")
+        require(Service)
+    end
+end
 
 Knit.Start():andThen(function()
     warn("Server Started")
